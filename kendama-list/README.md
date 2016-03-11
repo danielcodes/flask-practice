792c078130ded69ef2908a5142c343792e01e519
Daniel Chia <daniel_1118@hotmail.com>
2016-03-10 22:36:22 -0500

    added basic Kendama model

--- kendama-list/README.md
+++ kendama-list/README.md
@@ -7,6 +7,7 @@ Primarily starting this app to work with WTForms
 ## User stories
 
 * [ ] The main page displays a 3 column grid that displays pictures of kendamas
+* [ ] Under the image, there is a header with the name and a table with further description, brand, link to buy
 * [ ] There is a form, that allows me to add a name and link to the kendama picture
 
 
--- kendama-list/app.py
+++ kendama-list/app.py
@@ -1,5 +1,35 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+# =============================== MODELS ===============================
+# taken from the basic app, so db is created in this same dir
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI 
+
+db = SQLAlchemy(app)
+
+class Kendama(db.Model):
+    # link has to be validated as a url in the form
+    # Name | Brand | Link | Description
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    brand = db.Column(db.String(50))
+    link = db.Column(db.String(100))
+    description = db.Column(db.Text())
+
+    def __init__(self, name, brand, link, description):
+        self.name = name
+        self.brand = brand
+        self.description = description
+        self.link = link
+
+    def __repr__(self):
+        return '<Kendama %r>' % self.name
+
+# =============================== VIEWS ===============================
 
 @app.route("/")
 def hello():
@@ -8,10 +38,14 @@ def hello():
 
 @app.route("/kendamas")
 def kendama_list():
-    
     return "Hey, you currently have no kendamas"
+
+
+
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
